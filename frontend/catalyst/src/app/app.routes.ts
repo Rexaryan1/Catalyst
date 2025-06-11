e320437e7a28cb9f0c8ea0093e66699db5668580
Aryan Vohra <141185891+Rexaryan1@users.noreply.github.com>
2025-06-11 21:42:32 +0000

    Question Card

--- frontend/catalyst/src/app/app.routes.ts
+++ frontend/catalyst/src/app/app.routes.ts
@@ -1,7 +1,9 @@
     import { NgModule } from '@angular/core';
     import { RouterModule, Routes } from '@angular/router';
     import { LoginPageComponent } from './login-page/login-page.component';
+import { QuestionCardComponent } from './question-card/question-card.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
+    {path: 'questionCard',component: QuestionCardComponent},
 ];
