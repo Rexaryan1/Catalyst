28f43aac49dd44f388b7775e44e0ac3d66ab67f1
Saksham20402 <saksham20402@iiitd.ac.in>
2025-06-19 19:05:41 +0530

    Redoing server for authentication

--- servers/Python-Django/Catalyst/settings.py
+++ servers/Python-Django/Catalyst/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authentication",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -145,6 +147,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR/ 'static')]
+
+AUTH_USER_MODEL = "authentication.CustomUser"
+
 
 
 
