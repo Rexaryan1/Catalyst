d5cbe41f96e4f5e4411dc91472c0a5a0ea19d7b5
Aryan Vohra <141185891+Rexaryan1@users.noreply.github.com>
2025-06-22 10:01:37 +0000

    Login button working

--- frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
+++ frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { LoginPageComponent } from "../login-page/login-page.component";
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [LoginPageComponent, NgIf
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  showLoginPage = signal<boolean>(false);
 
+  renderLogin(): void {
+    // Logic to render the login page can be added here
+    this.toggleLoginVisibility();
+    const overlay = document.getElementById('overlay');
+    if (overlay) {
+      overlay.style.display = 'block';
+    }
+  }
+
+    ///* Helper functions to close the login page */
+  toggleLoginVisibility() {
+    this.showLoginPage.set(!this.showLoginPage());
+  }
 }
