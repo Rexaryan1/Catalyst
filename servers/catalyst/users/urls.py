742c786a6e45f1092213376b4f436d8c95d2cd06
Saksham20402 <88622260+Saksham20402@users.noreply.github.com>
2025-06-23 23:23:35 +0530

    Merge pull request #8
    * Auth with cookies
    
    * Logout added

---
+++ servers/catalyst/users/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register", views.RegisterView.as_view(), name="register"),
+    path("login" , views.LoginView.as_view(), name = "login"),
+    path("user" , views.UserView.as_view()),
+    path("logout" , views.LogoutView.as_view())
+]
