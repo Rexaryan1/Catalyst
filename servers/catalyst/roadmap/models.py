3a5c24762c9505ae37549dc04d808295fc6e1574
Saksham20402 <saksham20402@iiitd.ac.in>
2025-06-24 19:47:12 +0530

    Minor Changes

--- servers/catalyst/roadmap/models.py
+++ servers/catalyst/roadmap/models.py
@@ -1,3 +1,37 @@
+import uuid
+from django.db import models
+from question.models import Question
+from users.models import User
+
+
+class Roadmap(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'roadmaps'
+        verbose_name = "Roadmap"
+        verbose_name_plural = "Roadmaps"
+
+    def __str__(self):
+        return self.title
+
+
+class RoadmapQuestion(models.Model):
+    roadmap = models.ForeignKey(Roadmap, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    position = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'roadmap_question'
+        unique_together = (('roadmap', 'question'),)
+        managed = False
+
+
 from django.db import models
 
 # Create your models here.
