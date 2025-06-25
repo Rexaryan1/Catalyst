4c0299eecb692e2d5acd8d6ecb2648cc3901e21a
rexaryan1 <aryanvohra2002@gmail.com>
2025-06-26 03:11:37 +0530

    Login page integration with stupid overlay

--- frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
+++ frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { LoginPageComponent } from "../login-page/login-page.component";
 import { NgIf } from '@angular/common';
+import e from 'express';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -21,8 +22,18 @@ export class NavBarComponent {
     }
   }
 
-    ///* Helper functions to close the login page */
+  ///* Helper functions to close the login page */
   toggleLoginVisibility() {
     this.showLoginPage.set(!this.showLoginPage());
+    if (this.showLoginPage()) {
+      const overlay = document.getElementById('overlay');
+      if (overlay) {
+        overlay.style.display = 'block';
+      }
+      overlay?.addEventListener('click', () => {
+        this.toggleLoginVisibility();
+        overlay.style.display = 'none';
+      });
+    }
   }
 }
