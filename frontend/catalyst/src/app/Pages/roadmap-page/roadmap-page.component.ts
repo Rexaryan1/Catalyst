5a6a9b587f95b2729ca5dbb7adb8102a78afcb69
rexaryan1 <aryanvohra2002@gmail.com>
2025-07-27 21:40:08 +0530

    Saksham Code Fix

--- frontend/catalyst/src/app/Pages/roadmap-page/roadmap-page.component.ts
+++ frontend/catalyst/src/app/Pages/roadmap-page/roadmap-page.component.ts
@@ -1,7 +1,7 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{MindMapComponent, MindMapNode} from "@cards/mind-map/mind-map.component";
+import { MindMapComponent, MindMapNode } from "@app/cards/mind-map/mind-map.component";
 
 
 
