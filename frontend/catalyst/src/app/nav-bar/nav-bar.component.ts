8d418db0a47b2a9a09d15b8f60c7cae3c5c06304
rexaryan1 <aryanvohra2002@gmail.com>
2025-12-10 23:56:15 +0530

    login page changes

--- frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
+++ frontend/catalyst/src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,17 @@
 import { Component, signal, viewChild, ViewContainerRef } from '@angular/core';
-import { LoginPageComponent } from "../components/cards/login-card/login-page.component";
+// import { LoginPageComponent } from "../components/cards/login-card/login-page.component";
 import { NgIf } from '@angular/common';
 import { DataManagerService } from '@services/data-manager/data-manager.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [LoginPageComponent, NgIf
-  ],
+  imports: [NgIf, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-  showLoginPage = signal<boolean>(false);
 
   userName = '';
   userAvatar = 'assets/aryan.jpg';
@@ -28,23 +27,4 @@ export class NavBarComponent {
     });
   }
 
-  renderLogin(): void {
-    // Logic to render the login page can be added here
-    this.toggleLoginVisibility();
-  }
-
-  ///* Helper functions to close the login page and Overlay */
-  toggleLoginVisibility() {
-    this.showLoginPage.set(!this.showLoginPage());
-    if (this.showLoginPage()) {
-      const overlay = document.getElementById('overlay');
-      if (overlay) {
-        overlay.style.display = 'block';
-      }
-      overlay?.addEventListener('click', () => {
-        this.showLoginPage.set(false);
-        overlay.style.display = 'none';
-      });
-    }
-  }
 }
