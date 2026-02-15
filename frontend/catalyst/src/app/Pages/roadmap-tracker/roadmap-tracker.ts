3a0962df468b00f134cb77802d6f7c09feac4ebc
rexaryan1 <aryanvohra2002@gmail.com>
2026-02-15 16:26:56 +0530

    search integratiion

--- frontend/catalyst/src/app/Pages/roadmap-tracker/roadmap-tracker.ts
+++ frontend/catalyst/src/app/Pages/roadmap-tracker/roadmap-tracker.ts
@@ -28,39 +28,17 @@ export interface Roadmap {
   styleUrls: ['./roadmap-tracker.scss']
 })
 export class RoadmapTrackerComponent {
+  constructor(private dataManager: DataManagerService) { }
   activeTab: string = 'in-progress';
   searchTerm: string = '';
-
-
-  constructor(private dataManager: DataManagerService) { }
+  roadmaps: Roadmap[] = [];
 
   ngOnInit(): void {
     // Fetch roadmaps from backend API and populate the roadmaps array
-    this.dataManager.post('/roadmap/roadmap-list', { "search": "", "filters": {} }, { withCredentials: true }).subscribe({
+    this.dataManager.post('/roadmap/roadmap-list', { "search": this.searchTerm, "filters": {} }, { withCredentials: true }).subscribe({
       next: (res: any) => {
         // Assuming the response is an array of roadmaps
-        this.roadmaps = res.response.map((roadmap: any) => {
-
-          if (roadmap.avg_difficulty === 'Easy') {
-            roadmap.difficulty = DifficultyLevel.EASY;
-          } else if (roadmap.avg_difficulty === 'Medium') {
-            roadmap.difficulty = DifficultyLevel.MEDIUM;
-          } else if (roadmap.avg_difficulty === 'Hard') {
-            roadmap.difficulty = DifficultyLevel.HARD;
-          }
-
-          roadmap.progress_percntg = Math.round(roadmap.progress_percntg);
-
-          return {
-            id: roadmap.id,
-            title: roadmap.title,
-            date: roadmap.date,
-            summary: roadmap.description,
-            difficulty: roadmap.difficulty,
-            completion: roadmap.progress_percntg,
-            keywords: roadmap.preview_topics
-          }
-        });
+        this.processResponse(res);
         // this.roadmaps = this.roadmaps.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
       },
       error: (err) => {
@@ -68,93 +46,50 @@ export class RoadmapTrackerComponent {
       }
     });
   }
-  roadmaps: Roadmap[] = [
-    {
-      id: 1,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:09PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.EASY,
-      completion: 26,
-      keywords: ['keywords', 'keywords']
-    },
-    {
-      id: 2,
-      title: '#ROADMAP 2',
-      date: 'JAN 25 - 10:09PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.MEDIUM,
-      completion: 36,
-      keywords: ['keywords', 'keywords']
-    },
-    {
-      id: 3,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:09PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.HARD,
-      completion: 26,
-      keywords: ['keywords']
-    },
-    {
-      id: 4,
-      title: '#ROADMAP 2',
-      date: 'JAN 25 - 10:22PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.EASY,
-      completion: 26,
-      keywords: ['keywords']
-    },
-    {
-      id: 5,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:22PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.HARD,
-      completion: 76,
-      keywords: ['keywords', 'keywords']
-    },
-    {
-      id: 6,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:22PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.MEDIUM,
-      completion: 6,
-      keywords: ['keywords', 'keywords']
-    },
-    {
-      id: 3,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:09PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.HARD,
-      completion: 26,
-      keywords: ['keywords']
-    },
-    {
-      id: 4,
-      title: '#ROADMAP 2',
-      date: 'JAN 25 - 10:22PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.EASY,
-      completion: 26,
-      keywords: ['keywords']
-    },
-    {
-      id: 5,
-      title: '#ROADMAP 1',
-      date: 'JAN 25 - 10:22PM',
-      summary: 'Consectetur flatten the curve quaranteam so',
-      difficulty: DifficultyLevel.HARD,
-      completion: 76,
-      keywords: ['keywords', 'keywords']
-    },
-  ];
+
+  private processResponse(res: any) {
+    this.roadmaps = res.response.map((roadmap: any) => {
+
+      if (roadmap.avg_difficulty === 'Easy') {
+        roadmap.difficulty = DifficultyLevel.EASY;
+      } else if (roadmap.avg_difficulty === 'Medium') {
+        roadmap.difficulty = DifficultyLevel.MEDIUM;
+      } else if (roadmap.avg_difficulty === 'Hard') {
+        roadmap.difficulty = DifficultyLevel.HARD;
+      }
+
+      roadmap.progress_percntg = Math.round(roadmap.progress_percntg);
+
+      return {
+        id: roadmap.id,
+        title: roadmap.title,
+        date: roadmap.date,
+        summary: roadmap.description,
+        difficulty: roadmap.difficulty,
+        completion: roadmap.progress_percntg,
+        keywords: roadmap.preview_topics
+      };
+    });
+  }
+
+  onSearchChange(searchValue: string): void {
+    this.dataManager.post('/roadmap/roadmap-list', { "search": searchValue, "filters": {} }, { withCredentials: true }).subscribe({
+      next: (res: any) => {
+        this.processResponse(res);
+      },
+      error: (err) => {
+        console.error('Error fetching roadmaps:', err);
+      }
+    });
+  }
+
 
   setActiveTab(tab: string): void {
     this.activeTab = tab;
   }
+
+
+  //#region Helper Methods for Template
 
   getDifficultyColor(difficulty: DifficultyLevel): string {
     switch (difficulty) {
