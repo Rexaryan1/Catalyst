fb77bcb3eb8df09ddff43c5ce2814f210ab90ae3
rexaryan1 <aryanvohra2002@gmail.com>
2026-03-06 19:04:10 +0530

    route clean up

--- frontend/catalyst/src/app/app.routes.ts
+++ frontend/catalyst/src/app/app.routes.ts
@@ -1,23 +1,15 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './Pages/user-profile/user-profile.component';
 import { LandingPage } from '@pages/landing-page/landing-page.component';
 import { RoadmapPageComponent } from "@pages/roadmap-page/roadmap-page.component";
 import { UserDashboardComponent } from "@pages/user-dashboard/user-dashboard.component";
-import { RoadmapBlockComponent } from "@components/cards/roadmap-block/roadmap-block.component";
-import { ScoreCardComponent } from "@components/cards/profile-summary/score-card/score-card.component";
-import { ProfileSummaryComponent } from "@components/cards/profile-summary/profile-summary.component";
-import { PingBoardComponent } from "@components/cards/ping-board/ping-board.component";
-import { SolutionComponent } from "@components/cards/solution/solution.component";
 import { RoadmapWizardComponent } from "@components/roadmap-wizard/roadmap-wizard.component";
 import { MainPage } from "@pages/signup-page/signup-page";
 import { RoadmapListComponent } from "@pages/roadmap-page/roadmap-list/roadmap-list.component";
+//TODO : Chheck if this is requrired
 import { SolutionPage } from "@pages/solution-page/solution-page";
+
 import { RoadmapTrackerComponent } from './Pages/roadmap-tracker/roadmap-tracker';
-import { HeatmapSmall } from "@components/cards/heatmap-small/heatmap-small";
-import {
-  LandingDashboardCardComponent
-} from "@pages/landing-page/landing-dashboard-card/landing-dashboard-card.component";
 import { ProgressDashboardComponent } from './Pages/reports/progress-dashboard';
 import { StickyNav } from "@components/sticky-nav/sticky-nav";
 
@@ -30,10 +22,11 @@ export const routes: Routes = [
   { path: 'prompt', component: RoadmapWizardComponent },
   { path: 'dashboard', component: UserDashboardComponent },
   { path: 'userHome', component: UserProfileComponent },
-  //TODO: change to remmove
-  // { path: 'plan', component: StudyPlanComponent },
   { path: 'roadmap-tracker', component: RoadmapTrackerComponent },
   { path: 'roadmap', component: RoadmapPageComponent },
-  { path: 'preview', component: StickyNav },
   { path: 'progress', component: ProgressDashboardComponent },
+
+  // Preview for testing
+  { path: 'preview', component: StickyNav },
+
 ];
