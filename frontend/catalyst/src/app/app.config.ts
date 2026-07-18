956699f800aaa39b0682bac47823ac2554155054
Saksham20402 <88622260+Saksham20402@users.noreply.github.com>
2026-07-18 13:39:58 +0530

    routing changes for sessions and auth (#53)
    * routing changes for sessions and auth
    
    * Clean up app.config.ts by removing unused code
    
    Removed unused imports and app initializer for DataManagerService.
    
    ---------
    
    Co-authored-by: Aryan Vohra <141185891+Rexaryan1@users.noreply.github.com>

--- frontend/catalyst/src/app/app.config.ts
+++ frontend/catalyst/src/app/app.config.ts
@@ -8,19 +8,13 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { environment } from '@environments/environment';
 import { provideLottieOptions } from 'ngx-lottie';
 import player from 'lottie-web';
-//import { provideServiceWorker } from '@angular/service-worker';
-// This file is used to configure the application with the necessary providers and routes.
-// It sets up the router with the defined routes for the application.
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideLottieOptions({
       player: () => player,
     }),
     provideRouter(routes), provideAnimationsAsync(), provideHttpClient(),
-    // provideAppInitializer(() => {
-    //     const initializerFn = ((dataManager: DataManagerService) => () => dataManager.checkLoggedInStatus())(inject(DataManagerService));
-    //     return initializerFn();
-    //   }),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
